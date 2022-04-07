9f96ee70adfb8354f644c857d2bbaf74f799feac
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2022-04-07 21:58:48 +0530

    Classification using CNN

---
+++ Sgp_internal_1.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 19:43:16 2022
+
+@author: sjhan
+"""
+
+import keras
+import tensorflow
+import os
+from keras.layers import Input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from keras.layers import BatchNormalization
+from tensorflow.keras import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+from keras.models import Model
+from sklearn.utils import shuffle
+from cv2 import imread
+import numpy as np
+import pandas as pd
+
+#Loading data
+
+data = []
+labels = []
+width,height=150,150
+
+imagePaths = list(paths.list_images('gaussian_filtered_images/gaussian_filtered_images'))
+
+
+for imagePath in imagePaths:
+    label = imagePath.split(os.path.sep)[-2]
+    #print(imagePath)
+    image = load_img(imagePath, target_size=(width, height))
+    image = img_to_array(image)
+    data.append(image)
+    labels.append(label)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+#labels = to_categorical(labels)
+
+data, labels = shuffle(data, labels)
+
+print(data.shape)
+print(labels.shape)
+
+#Split into train test split
+
+test_ratio = 0.30
+
+# train is now 75% of the entire data set
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=test_ratio)
+
+print("Train images:",x_train.shape)
+print("Test images:",x_test.shape)
+print("Train label:",y_train.shape)
+print("Test label:",y_test.shape)
+
+INIT_LR = 1e-4
+EPOCHS = 25
+BS = 32
+
+cnn_model=Sequential()
+cnn_model=Sequential()
+cnn_model.add(Conv2D(16, (3, 3),activation='relu',input_shape=(150, 150, 3)))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(32, (3, 3),activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(64, (3, 3),activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(128, (3, 3), activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(256, (3, 3), activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(BatchNormalization())
+cnn_model.add(Flatten())   #Features Are Extracted From this Layer
+cnn_model.add(Dropout(0.2))
+cnn_model.add(Dense(1024, activation='relu'))
+cnn_model.add(Dense(5, activation='sigmoid'))
+
+opt = Adam(learning_rate=INIT_LR)
+cnn_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+cnn_model.summary()
+
+
+# train the head of the network
+print("[INFO] training head..")
+h = cnn_model.fit(x_train,y_train,epochs=10)
+print("Done !!")
+
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import numpy as np
+import scikitplot as skplt
+from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn import metrics
+
+print("[INFO] evaluating network...")
+predIdxs = cnn_model.predict(x_test, batch_size=BS)
+predIdxs = np.argmax(predIdxs, axis=1)
+
+trainpredIdxs = cnn_model.predict(x_train, batch_size=BS)
+trainpredIdxs = np.argmax(trainpredIdxs, axis=1)
+
+trainCNNScore=accuracy_score(trainpredIdxs,y_train.argmax(axis=1))*100
+CNNScore=accuracy_score(predIdxs,y_test.argmax(axis=1))*100
+
+print("\nTrainig Accuracy Score:-",trainCNNScore)
+print("\nTesting Accuracy Score:-",CNNScore)
+
+
+val_loss,val_acc=cnn_model.evaluate(x_test,y_test)
+print(val_loss)
+print(val_acc)
