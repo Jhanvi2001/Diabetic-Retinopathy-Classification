6957bf3cae0c353951116d17d6792389a93b7dea
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2022-04-15 11:50:52 +0530

    Using CNN

---
+++ Using_CNN.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 19:43:16 2022
+
+@author: sjhan
+"""
+
+import keras
+import tensorflow
+import os
+from keras.layers import Input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from keras.layers import BatchNormalization
+from tensorflow.keras import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+from keras.models import Model
+from sklearn.utils import shuffle
+from cv2 import imread
+import numpy as np
+import pandas as pd
+
+#Loading data
+
+data = []
+labels = []
+width,height=150,150
+
+imagePaths = list(paths.list_images('gaussian_filtered_images/gaussian_filtered_images'))
+
+
+for imagePath in imagePaths:
+    label = imagePath.split(os.path.sep)[-2]
+    #print(imagePath)
+    image = load_img(imagePath, target_size=(width, height))
+    image = img_to_array(image)
+    data.append(image)
+    labels.append(label)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+#labels = to_categorical(labels)
+
+data, labels = shuffle(data, labels)
+
+print(data.shape)
+print(labels.shape)
+
+#Split into train test split
+
+test_ratio = 0.40
+
+# train is now 75% of the entire data set
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=test_ratio)
+
+print("Train images:",x_train.shape)
+print("Test images:",x_test.shape)
+print("Train label:",y_train.shape)
+print("Test label:",y_test.shape)
+
+INIT_LR = 1e-4
+BS =32
+
+
+cnn_model=Sequential()
+cnn_model.add(Conv2D(20, (3, 3),activation='relu',input_shape=(150, 150, 3)))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(40, (3, 3),activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(80, (3, 3),activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(160, (3, 3), activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(Conv2D(320, (3, 3), activation='relu'))
+cnn_model.add(MaxPooling2D(2,2))
+cnn_model.add(BatchNormalization())
+cnn_model.add(Flatten())   #Features Are Extracted From this Layer
+cnn_model.add(Dropout(0.2))
+cnn_model.add(Dense(1024, activation='relu'))
+cnn_model.add(Dense(5, activation='sigmoid'))
+
+opt = Adam(learning_rate=INIT_LR)
+cnn_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+cnn_model.summary()
+
+
+# train the head of the network
+print("[INFO] training head..")
+h = cnn_model.fit(x_train,y_train,epochs=20)
+print("Done !!")
+
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import numpy as np
+import scikitplot as skplt
+from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn import metrics
+
+print("[INFO] evaluating network...")
+predIdxs = cnn_model.predict(x_test, batch_size=BS)
+predIdxs = np.argmax(predIdxs, axis=1)
+
+trainpredIdxs = cnn_model.predict(x_train, batch_size=BS)
+trainpredIdxs = np.argmax(trainpredIdxs, axis=1)
+
+trainCNNScore=accuracy_score(trainpredIdxs,y_train.argmax(axis=1))*100
+CNNScore=accuracy_score(predIdxs,y_test.argmax(axis=1))*100
+
+print("\nTrainig Accuracy Score:-",trainCNNScore)
+print("\nTesting Accuracy Score:-",CNNScore)
+
+
+val_loss,val_acc=cnn_model.evaluate(x_test,y_test)
+print(val_loss)
+print(val_acc)
+
+####
+extractCNN = Model(cnn_model.inputs, cnn_model.layers[-4].output)
+
+#del(data)
+#del(labels)
+feat_trainCNN  = extractCNN.predict(x_train)  
+feat_testCNN = extractCNN.predict(x_test)      
+
+print(feat_trainCNN.shape)
+
+###################SVM##################
+
+from sklearn.svm import SVC
+
+svm = SVC(kernel='linear')
+svm.fit(feat_trainCNN,np.argmax(y_train,axis=1))
+
+TrainSVMScoreCNN=svm.score(feat_trainCNN,np.argmax(y_train,axis=1))*100
+print("SVM Training Accuracy Score:-",TrainSVMScoreCNN)
+
+TestSVMScoreCNN=svm.score(feat_testCNN,np.argmax(y_test,axis=1))*100
+print("\nSVM Testing Accuracy Score:-",TestSVMScoreCNN)
+
+
+# ################KNN####################
+# from sklearn.neighbors import KNeighborsClassifier
+
+# knn = KNeighborsClassifier(n_neighbors=5)
+# knn.fit(feat_trainCNN,np.argmax(y_train,axis=1))
+
+# TrainKNNScoreCNN=knn.score(feat_trainCNN,np.argmax(y_train,axis=1))*100
+# print("KNN Training Accuracy Score:-",TrainKNNScoreCNN)
+
+# TestKNNScoreCNN=knn.score(feat_testCNN,np.argmax(y_test,axis=1))*100
+# print("\nKNN Testing Accuracy Score:-",TestKNNScoreCNN)
+
+# ####################Decision Tree##############
+# from sklearn.tree import DecisionTreeClassifier
+
+# clf = DecisionTreeClassifier(random_state=0)
+# clf = clf.fit(feat_trainCNN,np.argmax(y_train,axis=1))
+
+# TrainDecisionScoreCNN=clf.score(feat_trainCNN,np.argmax(y_train,axis=1))*100
+# print("Decision Tree Training Accuracy Score:-",TrainDecisionScoreCNN)
+
+
+# TestDecisionScoreCNN=clf.score(feat_testCNN,np.argmax(y_test,axis=1))*100
+# print("\nDecision Tree Testing Accuracy Score:-",TestDecisionScoreCNN)
+
+# #####################Random Forest###############################
+# from sklearn.naive_bayes import GaussianNB
+
+# gnb = GaussianNB()
+# gnb.fit(feat_trainCNN,np.argmax(y_train,axis=1))
+
+# TrainNBScoreCNN=gnb.score(feat_trainCNN,np.argmax(y_train,axis=1))*100
+# print("\nGaussianNaive Bayes Training Accuracy Score:-",TrainNBScoreCNN)
+
+# TestNBScoreCNN=gnb.score(feat_testCNN,np.argmax(y_test,axis=1))*100
+# print("\nGaussianNaive Bayes Testing Accuracy Score:-",TestNBScoreCNN)
